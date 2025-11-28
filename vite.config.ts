b1ea677dd43250199ac988ab547ac59ecf0bdd98
experdot <experdot@foxmail.com>
2025-11-28 22:39:16 +0800

    chore: add github actions & remove unused env vars

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,17 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.API_KEY),
-        'process.env.API_HOST': JSON.stringify(env.API_HOST),
-        'process.env.API_MODEL': JSON.stringify(env.API_MODEL),
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  base: '/card-world/',
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
